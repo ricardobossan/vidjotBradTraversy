09082141766ad8035765f06b3493beb17180116f
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2019-06-11 17:38:57 -0300

    S4-21

--- app.js
+++ app.js
@@ -41,9 +41,27 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
-// Add Idea Route
+// Idea Index Page
+app.get('/ideas', (req, res) => {
+  Idea.find({})
+    .sort({ date: 'desc' })
+    .then(ideas => {
+      res.render('ideas/index', { ideas: ideas });
+    });
+});
+
+// Add Idea Form
 app.get('/ideas/add', (req, res) => {
   res.render('ideas/add');
+});
+
+// Edit Idea Form
+app.get('/ideas/edit/:id', (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  }).then(idea => {
+    res.render('ideas/edit', { idea: idea });
+  });
 });
 
 // Process Form
